a63d4cfcb466bad0f8cdf91cf063ba8f68e4aa40
ekiscrim <xcstudiosonly@gmail.com>
2024-05-16 23:25:41 +0200

    5 - Frontend Styles with Tailwind

---
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+//import XSvg from "../../../components/svgs/X";
+
+//import { MdOutlineMail } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
+
+const LoginPage = () => {
+	const [formData, setFormData] = useState({
+		username: "",
+		password: "",
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formData);
+	};
+
+	const handleInputChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	const isError = false;
+
+	return (
+		<div className='max-w-screen-xl mx-auto flex h-screen'>
+			<div className='flex-1 hidden lg:flex items-center  justify-center'>
+				{/*<XSvg className='lg:w-2/3 fill-white' />*/}
+			</div>
+			<div className='flex-1 flex flex-col justify-center items-center'>
+				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
+					{/*<XSvg className='w-24 lg:hidden fill-white' />*/}
+					<h1 className='text-4xl font-extrabold text-primary'>{"Let's"} go.</h1>
+					<label className='input input-bordered rounded flex items-center gap-2'>
+            <FaUser />
+						<input
+							type='text'
+							className='grow'
+							placeholder='Nombre de usuario'
+							name='username'
+							onChange={handleInputChange}
+							value={formData.username}
+						/>
+					</label>
+
+					<label className='input input-bordered rounded flex items-center gap-2'>
+						<MdPassword />
+						<input
+							type='password'
+							className='grow'
+							placeholder='Contraseña'
+							name='password'
+							onChange={handleInputChange}
+							value={formData.password}
+						/>
+					</label>
+					<button className='btn rounded-full btn-primary text-white'>Acceder</button>
+					{isError && <p className='text-red-500'>Algo fue mal</p>}
+				</form>
+				<div className='flex flex-col gap-2 mt-4'>
+					<p className='text-primary text-lg'>{"¿No"} tienes cuenta?</p>
+					<Link to='/register'>
+						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Ir a registrarte</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default LoginPage;
