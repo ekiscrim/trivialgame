ded46a3e8336ade722ea72b24022aeee5cdaa8cd
ekiscrim <xcstudiosonly@gmail.com>
2024-06-20 16:44:28 +0200

    110. Storage session in mongo

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,13 +1,26 @@
+// connectMongoDB.js
 import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const connectMongoDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Conectado a MongoDB: ${conn.connection.host}`);
+        await mongoose.connect(process.env.MONGO_URI);
+
+        const mongoStore = MongoStore.create({
+            mongoUrl: process.env.MONGO_URI, // URL de conexión a MongoDB
+            collectionName: "sessions", // Nombre de la colección donde se guardarán las sesiones
+            ttl: 7 * 24 * 60 * 60, // Tiempo de vida de la sesión en segundos
+            autoRemove: "native", // Opción adicional para configurar el auto-remover
+            mongoOptions: { } // Opciones de conexión de MongoDB
+        });
+
+        console.log(`Conectado a MongoDB: ${mongoose.connection.host}`);
+        return mongoStore; // Devuelve la instancia de MongoStore
     } catch (error) {
         console.error(`Error al conectarse a MongoDB: ${error.message}`);
         process.exit(1);
     }
-}
+};
 
 export default connectMongoDB;
